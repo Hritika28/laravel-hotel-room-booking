94adde592e6a6c8702c8bb26d8a1f317b65a7285
Hritika Mathur <hritikamathur@BGINDLM14LP0242.local>
2025-09-02 12:09:48 +0530

    final commit -- removing not required files

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
 
-Route::get('/rooms', [BookingController::class, 'index'])->name('rooms');
+Route::get('/', [BookingController::class, 'index'])->name('rooms');
 Route::post('/booking', [BookingController::class, 'book'])->name('book');
 Route::post('/reset', [BookingController::class, 'reset'])->name('reset');
 Route::post('/random', [BookingController::class, 'random_book'])->name('random');
