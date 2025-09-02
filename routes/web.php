29d89ecbe3741d890b69215cce75e6fc97de57ae
Hritika Mathur <hritikamathur@192.168.1.9>
2025-09-02 13:44:26 +0530

    added seeding route

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookingController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', [BookingController::class, 'index'])->name('rooms');
 Route::post('/booking', [BookingController::class, 'book'])->name('book');
 Route::post('/reset', [BookingController::class, 'reset'])->name('reset');
 Route::post('/random', [BookingController::class, 'random_book'])->name('random');
+
+Route::get('/run-seeders', function () {
+    Artisan::call('db:seed', ['--force' => true]);
+    return 'Seeding complete!';
+});
